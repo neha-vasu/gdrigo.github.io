3d3223dc43a2cfb1b37513645fb462258bee10f1
gdrigo <gkd244@nyu.edu>
2020-03-18 15:26:25 -0400

    hella additions. record grid page done.

--- webDev/midtermProposalV3/recordCollection.js
+++ webDev/midtermProposalV3/recordCollection.js
@@ -1,69 +1,57 @@
-function showDetails(evt, albumName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(albumName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
+/////////////////////
+//FOR recordlist.html
 
-
-function showDetails(albumName, artistName) {
-  let artistNS = document.getElementsByClassName("artistNameSpace");
-  let albumNS = document.getElementsByClassName("albumNameSpace");
+//adds the album and artist names to the sides of the grid of albums when one is hovered over 
+function showDetails(albumName, artistName, textColor) {
+  let artistNS = document.getElementById("artistNameSpace");
+  let albumNS = document.getElementById("albumNameSpace");
 
   //add artistName to artistNameSpace div
-  let artName = document.createElement("p");
-  let artText = document.createTextNode(artistName);
-  artName.appendChild(artText);
-  artistNS.appendChild(artName);
+  let artistPara = document.createElement("p");
+  let artistText = document.createTextNode(artistName);
+  artistPara.appendChild(artistText);
+  artistNS.appendChild(artistPara);
 
   //add albumName to albumNameSpace div
-  let albName = document.createElement("p");
-  let albText = document.createTextNode(albumName);
-  artName.appendChild(albText);
-  artistNS.appendChild(albName);
+  let albumPara = document.createElement("p");
+  let albumText = document.createTextNode(albumName);
+  albumPara.appendChild(albumText);
+  albumNS.appendChild(albumPara);
+
+  //add styling to album and artist names
+  addStyling(artistNS.firstChild, textColor);
+  addStyling(albumNS.firstChild, textColor);
 }
 
- 
+//makes the album name and artist the color of the album cover
+function  addStyling(artistOrAlbumText, textColor) {
+  let artistNS = document.getElementById("artistNameSpace");
+  let albumNS = document.getElementById("albumNameSpace");
+
+  artistOrAlbumText.style.color = textColor;
+  artistOrAlbumText.style.writingMode = "vertical-rl";
+  artistOrAlbumText.style.textOrientation = "sideways";
+  artistOrAlbumText.style.textAlign = "center";
+  artistOrAlbumText.style.fontSize = "4vw";
+}
+
+function removeDetails() {
+  let artistNS = document.getElementById("artistNameSpace");
+  let albumNS = document.getElementById("albumNameSpace");
+
+  while (artistNS.firstChild){
+    artistNS.removeChild(artistNS.firstChild); 
+  }
+
+  while (albumNS.firstChild){
+    albumNS.removeChild(albumNS.firstChild); 
+  }
+}
+//Link to onomouseover and onmouseleave: https://www.w3schools.com/jsref/event_onmouseover.asp
 
 
- // var tag = document.createElement("p");
- //   var text = document.createTextNode("Tutorix is the best e-learning platform");
- //   tag.appendChild(text);
- //   var element = document.getElementById("new");
- //   element.appendChild(tag);
 
 
 
-
-// let test = document.getElementById("test");
-  
-// // This handler will be executed only once when the cursor
-// // moves over the unordered list
-// test.addEventListener("mouseenter", function( event ) {   
-//   // highlight the mouseenter target
-//   event.target.style.color = "purple";
-
-//   // reset the color after a short delay
-//   setTimeout(function() {
-//     event.target.style.color = "";
-//   }, 500);
-// }, false);
-
-// // This handler will be executed every time the cursor
-// // is moved over a different list item
-// test.addEventListener("mouseover", function( event ) {   
-//   // highlight the mouseover target
-//   event.target.style.color = "orange";
-
-//   // reset the color after a short delay
-//   setTimeout(function() {
-//     event.target.style.color = "";
-//   }, 500);
-// }, false);
+//////////////////////////////
+//FOR whenicomehome.html
